42f394ba8d4f6afbc8fe88f92f7484aa1bb0b5e0
albertoarmienta <albertoarmienta@gmail.com>
2016-08-25 13:23:44 -0700

    Got base bootstrap template working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/view'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#view'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
